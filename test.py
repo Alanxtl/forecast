035397ce207b3a15675e9711eeb7680b18386f50
Alanxtl <m134679102365478@163.com>
2024-11-08 18:20:08 +0800

    add path check for new var

--- test.py
+++ test.py
@@ -6,8 +6,12 @@ import src.config as config
 conf = config.Config.get_config()
 logger.add(conf["log_path"] + "/{time}.log", level="DEBUG")
 
+if not os.path.exists(conf["data_path"]):
+    os.mkdir(conf["data_path"])
 if not os.path.exists(conf["raw_data_path"]):
     os.mkdir(conf["raw_data_path"])
+if not os.path.exists(conf["predict_data_path"]):
+    os.mkdir(conf["predict_data_path"])
 if not os.path.exists(conf["log_path"]):
     os.mkdir(conf["log_path"])
 
